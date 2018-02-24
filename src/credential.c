944293483aff8abb724de2f9bca1852c5c4381f7
Tiago Augusto Pimenta <tiagoapimenta@gmail.com>
2018-02-24 13:26:10 +0000

    Executing sudo if provided

--- src/credential.c
+++ src/credential.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <string.h>
 #include "io.h"
 
 void check_credential(int argc, char* argv[]) {
@@ -11,7 +12,12 @@ void check_credential(int argc, char* argv[]) {
 	}
 	sudo = which("sudo");
 	if (sudo) {
-		// exec sudo
+		char **args = malloc((argc + 3) * sizeof(char*));
+		args[0] = "sudo";
+		args[1] = "--";
+		memcpy(&args[2], argv, argc * sizeof(char*));
+		args[argc + 2] = NULL;
+		execv(sudo, args);
 	}
 	fprintf(stderr, "It requires to be run as root to work.\n");
 	exit(-1);
